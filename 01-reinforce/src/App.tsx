10c1d002c6b15da3951d627bafe2b5805eb5df95
Juan Camilo Lopez <juclopezso@unal.edu.co>
2021-05-26 14:54:26 -0500

    first commit

---
+++ 01-reinforce/src/App.tsx
@@ -0,0 +1,13 @@
+import { BasicTypes } from "./typescript/BasicTypes"
+
+const App = () => {
+  return (
+    <div className="mt-2">
+     <h1>Intro a typescript React</h1> 
+     <hr/>
+     <BasicTypes />
+    </div>
+  )
+}
+
+export default App;
---
+++ 01-reinforce/src/typescript/BasicTypes.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const BasicTypes = () => {
+    return (
+        <>
+          <h3>Tipos basicos</h3>  
+        </>
+    )
+}
