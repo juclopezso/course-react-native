a959133937fe55cefe96246a19b2cfde9ec70852
Juan Camilo Lopez <juclopezso@unal.edu.co>
2021-05-30 16:44:01 -0500

    floating action button FAB on counter

--- 02-FirstApp/src/screens/CounterScreen.tsx
+++ 02-FirstApp/src/screens/CounterScreen.tsx
@@ -1,36 +1,70 @@
 import React, { useState } from 'react'
-import { Button, Text, TouchableOpacity, View } from 'react-native'
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 export const CounterScreen = () => {
 
   const [counter, setCounter] = useState(10)
 
   return (
-    <View style={{
-      flex: 1, // use all the space possible of the parent
-      justifyContent: 'center'
-    }}>
-      <Text style={{
-        fontSize: 40,
-        textAlign: 'center',
-        // position: 'relative', // realtive by default
-        top: -10
-      }}>Contador {counter}</Text> 
+    <View style={styles.container}>
+      <Text style={styles.title}>Contador {counter}</Text> 
       {/* button doesn't have styles */}
       {/* <Button 
         title="Sumar" 
         onPress={() => setCounter(counter + 1)}
       /> */}
       <TouchableOpacity
+        style={styles.fabLocationBL}
+        onPress={() => setCounter(counter - 1)} 
+      >
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>-1</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.fabLocationBR}
         onPress={() => setCounter(counter + 1)} 
       >
-        <View style={{
-          backgroundColor: 'red',
-          borderRadius: 100,
-        }}>
-          <Text>+1</Text>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>+1</Text>
         </View>
       </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, // use all the space possible of the parent
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 40,
+    textAlign: 'center',
+    // position: 'relative', // realtive by default
+    top: -10
+  },
+  fabLocationBL: {
+    position: 'absolute',
+    left: 25,
+    bottom: 25,
+  },
+  fabLocationBR: {
+    position: 'absolute',
+    right: 25,
+    bottom: 25,
+  },
+  fab: {
+    backgroundColor: '#5856D6',
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    justifyContent: 'center'
+  },
+  fabText: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+    alignSelf: 'center'
+  }
+})
