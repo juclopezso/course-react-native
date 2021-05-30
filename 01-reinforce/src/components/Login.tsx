f7b53d88a13d440fea776a28091d350d9e66450f
Juan Camilo Lopez <juclopezso@unal.edu.co>
2021-05-30 07:07:10 -0500

    use reducer for login

--- 01-reinforce/src/components/Login.tsx
+++ 01-reinforce/src/components/Login.tsx
@@ -14,8 +14,15 @@ const initialState: AuthState = {
     name: ''
 }
 
+type LoginPayload = {
+  username: string;
+  name: string;
+}
+
 // type for actions
-type AuthAction = { type: 'logout' };
+type AuthAction = 
+    | { type: 'logout' }
+    | { type: 'login', payload: LoginPayload };
 
 // returns AuthState interface
 const authReducer = (state: AuthState, action: AuthAction): AuthState => {
@@ -28,6 +35,16 @@ const authReducer = (state: AuthState, action: AuthAction): AuthState => {
                 name: '',
                 username: ''
             }
+        case 'login':
+          const { name, username } = action.payload
+          return {
+            validating: false,
+            token: 'abc1234',
+            name,
+            username
+            // name: action.payload.name,
+            // username: action.payload.username
+          }
         default:
             return state;
     }
@@ -37,7 +54,7 @@ export const Login = () => {
 
     // const  [state, dispatch] = useReducer(authReducer, initialState)
     // como state es de tipo AuthState se puede desestructurar asi:
-    const  [{ validating }, dispatch] = useReducer(authReducer, initialState)
+    const  [{ validating, token, name }, dispatch] = useReducer(authReducer, initialState)
 
 
     useEffect(() => {
@@ -45,27 +62,59 @@ export const Login = () => {
             dispatch({ type: 'logout' })
         }, 1500)
     }, [])
+    // []: only executes useEffect at first render
+    // [state]: executes every time state changes
+
+    const login = () => {
+      dispatch({
+        type: 'login', 
+        payload: {
+          name: 'Juan',
+          username: 'Juanchozx'
+        } 
+      })
+    }
+
+    const logout = () => {
+      dispatch({ type: 'logout' })
+    }
 
     if (validating) {
         return (
-                <>
+          <>
+            <h3>Login</h3>
             <div className="alert alert-info">Validando...</div>
-            </>
+          </>
         )
     }
 
     return (
         <>
-            <h3>Login</h3>
-            
-            <div className="alert alert-danger">No autenticado...</div>
-            <div className="alert alert-success">Autenticado...</div>
-            <button className="btn btn-primary">
-                Login
-            </button>
-            <button className="btn btn-danger">
-                Logout
-            </button>
+          <h3>Login</h3>
+
+          {
+            ( token )
+              ? <div className="alert alert-success">Autenticado como: {name}</div>
+              : <div className="alert alert-danger">No autenticado...</div>
+          }
+
+          {
+            ( token ) 
+              ? (
+                <button 
+                  className="btn btn-danger"
+                  onClick={ logout }>
+                    Logout
+                </button>
+              )
+              : (
+                <button 
+                  className="btn btn-primary"
+                  onClick={ login }>
+                    Login
+                </button>
+              )
+          }
         </>
     )
 }
