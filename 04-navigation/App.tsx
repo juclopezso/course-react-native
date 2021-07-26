60a166c6c9d93def4b549cdd8a9da094a1ad0b64
Juan Camilo Lopez <juclopezso@unal.edu.co>
2021-07-25 21:43:36 -0500

    initial navigation project with init navigation

---
+++ 04-navigation/App.tsx
@@ -0,0 +1,17 @@
+import 'react-native-gesture-handler';
+import React from 'react'
+import { Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+
+
+const App = () => {
+  return (
+    // to create navigation
+    <NavigationContainer>
+      <Text>Hola</Text> 
+    </NavigationContainer>
+  )
+}
+
+export default App;
+
