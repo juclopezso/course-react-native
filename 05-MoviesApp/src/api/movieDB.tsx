6d721fbb688f4d077c8b66ddc57f303414972390
Juan Camilo Lopez <juclopezso@unal.edu.co>
2021-08-16 12:45:48 -0500

    movies app with carousel changed api

--- 05-MoviesApp/src/api/movieDB.tsx
+++ 05-MoviesApp/src/api/movieDB.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const movieDB = axios.create({
   baseURL: 'https://api.themoviedb.org/3/movie',
   params: {
-    api_key: 'a42743a01003005665be7586763b8d46',
+    api_key: 'another',
     language: 'es'
   }
 })
